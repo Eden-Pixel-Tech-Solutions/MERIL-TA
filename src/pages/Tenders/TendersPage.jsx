81737ea65dfcc3eafb6cba686a22379c03a076b5
Steve Jerald J B <jbStevejerald.official@gmail.com>
2025-12-16 23:46:41 +0530

    Front end state removed

--- src/pages/Tenders/TendersPage.jsx
+++ src/pages/Tenders/TendersPage.jsx
@@ -151,7 +151,7 @@ const TenderRow = ({ tender, serialNumber, onAction, onToggleInterest }) => {
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" /><circle cx="12" cy="10" r="3" />
           </svg>
-          {tender.department} - {tender.location}, {tender.state}
+          {tender.department}
         </div>
       </div>
 
